deb0c157d90aafe9207d77ffcebaf75e5af7ca45
SynTime <yqbyt0000@outlook.com>
2025-02-08 20:19:24 +0800

    remove target information

--- target/debug/deps/SortChineseName-65beffd3150b49c4.d
+++
@@ -1,5 +0,0 @@
-/Users/bubbles/Tools/SortChineseName/target/debug/deps/libSortChineseName-65beffd3150b49c4.rmeta: src/main.rs
-
-/Users/bubbles/Tools/SortChineseName/target/debug/deps/SortChineseName-65beffd3150b49c4.d: src/main.rs
-
-src/main.rs:
