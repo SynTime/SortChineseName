944f159c14f696a6065e5e8ad030dff26590fd47
SynTime <yqbyt0000@outlook.com>
2025-02-08 19:50:05 +0800

    delete target information

--- target/debug/build/proc-macro2-2ae8a198dc11c7e9/build_script_build-2ae8a198dc11c7e9.d
+++
@@ -1,5 +0,0 @@
-/Users/bubbles/Tools/SortChineseName/target/debug/build/proc-macro2-2ae8a198dc11c7e9/build_script_build-2ae8a198dc11c7e9: /Users/bubbles/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs
-
-/Users/bubbles/Tools/SortChineseName/target/debug/build/proc-macro2-2ae8a198dc11c7e9/build_script_build-2ae8a198dc11c7e9.d: /Users/bubbles/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs
-
-/Users/bubbles/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs:
