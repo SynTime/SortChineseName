ca017887a94ab4cc42727c56bcd63fc739769ef5
Syntime <yqbyt0000@outlook.comn>
2025-02-09 08:24:11 +0800

    delete compiler information

--- target/release/build/proc-macro2-50f0612dc63dd069/build_script_build-50f0612dc63dd069.d
+++
@@ -1,5 +0,0 @@
-/Users/bubbles/SortChineseName/target/release/build/proc-macro2-50f0612dc63dd069/build_script_build-50f0612dc63dd069: /Users/bubbles/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs
-
-/Users/bubbles/SortChineseName/target/release/build/proc-macro2-50f0612dc63dd069/build_script_build-50f0612dc63dd069.d: /Users/bubbles/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs
-
-/Users/bubbles/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.93/build.rs:
--- target/x86_64-pc-windows-gnu/release/SortChineseName.d
+++
@@ -1,1 +0,0 @@
-/Users/bubbles/SortChineseName/target/x86_64-pc-windows-gnu/release/SortChineseName.exe: /Users/bubbles/SortChineseName/src/main.rs
